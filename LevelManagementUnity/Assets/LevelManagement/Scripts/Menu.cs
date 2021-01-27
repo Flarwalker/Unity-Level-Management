d2b6aef02d10f6d29fce5268eea19ff265a247e6
Flarwalker <rfcross1207@gmail.com>
2021-01-26 20:46:58 -0500

    Lesson 17 Challenge Menu Methods

--- LevelManagementUnity/Assets/LevelManagement/Scripts/Menu.cs
+++ LevelManagementUnity/Assets/LevelManagement/Scripts/Menu.cs
@@ -7,7 +7,10 @@ namespace LevelManagement {
   [RequireComponent(typeof(Canvas))]
   public class Menu : MonoBehaviour {
     public void OnBackPressed () {
-
+      MenuManager menuManager = Object.FindObjectOfType<MenuManager>();
+      if (menuManager != null) {
+        menuManager.CloseMenu();
+      }
     }
 
     public void OnPlayPressed () {
@@ -18,11 +21,19 @@ namespace LevelManagement {
     }
 
     public void OnSettingsPressed () {
-
+      MenuManager menuManager = Object.FindObjectOfType<MenuManager>();
+      Menu settingsMenu = transform.parent.Find("SettingsMenu(Clone)").GetComponent<Menu>();
+      if (menuManager != null && settingsMenu != null) {
+        menuManager.OpenMenu(settingsMenu);
+      }
     }
 
     public void OnCreditsPressed () {
-
+      MenuManager menuManager = Object.FindObjectOfType<MenuManager>();
+      Menu creditsScreen = transform.parent.Find("CreditsScreen(Clone)").GetComponent<Menu>();
+      if (menuManager != null && creditsScreen != null) {
+        menuManager.OpenMenu(creditsScreen);
+      }
     }
   }
 }
